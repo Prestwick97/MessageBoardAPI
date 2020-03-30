8e1589a1261f196b935ab4984fad2c1a9e55d8fc
Prestwick97 <prestwick97@gmail.com>
2020-03-30 13:23:40 -0700

    Add DateTime field to message class
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>

---
+++ Migrations/20200330201748_ReDate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class ReDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Group",
+                table: "Messages",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Messages",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostDate",
+                table: "Messages",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostDate",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Group",
+                table: "Messages",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Messages",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+        }
+    }
+}
--- Models/Message.cs
+++ Models/Message.cs
@@ -1,12 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace MessageBoard.Models
 {
-    public class Message
-    {
-        public int MessageId { get; set; }
-        public string Content { get; set; }
-        public string Group { get; set; }
-        // public DateTime Date { get; set; }
-        public string UserName { get; set; }
-    }
+  public class Message
+  {
+    public int MessageId { get; set; }
+    [Required]
+    [StringLength(1000, ErrorMessage = "stop")]
+    public string Content { get; set; }
+    [Required]
+    [StringLength(25, ErrorMessage = "Don't")]
+    public string Group { get; set; }
+    // [DisplayFormat(DataFormatString="{0:MM/dd/yyyy}")]
+    public DateTime PostDate { get; set; }
+    public string UserName { get; set; }
+  }
 }
